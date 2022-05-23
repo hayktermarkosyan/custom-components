9714b7b0708aba2b50e07721b729422232282a78
hayktermarkosyan <hayktermarkosyan@gmail.com>
2022-05-23 14:23:30 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from './components/button/Button';
+import CloseIcon from './components/CloseIcon';
+import Modal from './components/modal/Modal';
 
 function App() {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button 
+        children="open modal" 
+        type="outline" 
+        btnColor='blue'
+        onClick={() => setShow(true)}
+      />
+
+      <Modal show={show} onClose={() => setShow(false)}>
+        <div className="content">
+          <Button 
+            children="blocked button" 
+            type="block" 
+            btnColor="grey"
+            labelColor='black'
+          />
+
+          <Button 
+            children="disabled button" 
+            disabled 
+            btnColor="red"
+            labelColor='black'
+          />
+
+          <Button 
+            children="rounded button" 
+            type="rounded" 
+            btnColor='yellow'
+            labelColor='red'
+          />
+
+          <Button 
+            children={
+                      <>
+                        <CloseIcon 
+                          width="20" 
+                          height="20" 
+                          style={{marginRight: "10px"}}
+                        />
+                        Close
+                      </>
+                      } 
+            type="outline" 
+            btnColor='blue'
+          />
+        </div>
+      </Modal>
     </div>
   );
 }
